23932105aea28722b2960a2ea6878c4111fa3304
udohufak <113771340+udohufak@users.noreply.github.com>
2023-05-08 21:32:15 +0100

    Update

--- int.js
+++ int.js
@@ -1,1 +1,15 @@
-//alert('Hello world')
+function search (){
+    document.getElementById('search-page').style.display = 'block';
+    
+}
+function deSearch(){
+    document.getElementById('search-page').style.display = 'none';
+    document.getElementById('search-page').style.animation = 'opaqueOut';
+
+}
+function show(){
+    document.getElementById('h-bedroom').style.visibility = 'visible'
+}
+function dshow(){
+    document.getElementById('h-bedroom').style.visibility = 'hidden'
+}
